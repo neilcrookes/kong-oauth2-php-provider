45895bd41ab116edccacfd6007b3a597faf1579b
Neil Crookes <neil.crookes@parcelmonkey.com>
2015-07-08 11:31:26 +0100

    Updates

---
+++ example/index.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Put this file in the document root of your application, adjust the settings below, then visit it in your browser
+ */
+
+require 'vendor/autoload.php';
+
+/**
+ * Just edit these settings:
+ */
+$provider = new Kong\OAuth2\Client\Provider\Kong([
+    'clientId'      => 'XXXX', // Get this from your app settings in Kong
+    'clientSecret'  => 'XXXX', // Get this from your app settings in Kong
+    'redirectUri'   => 'http://my-app-domain.com/', // This is the URL of your app, and must match what you added in Kong when you created the app
+    'storeAdminDomain' => 'my-test-store.kong365.com', // In reality you need to prompt the user to provide this
+]);
+
+/**
+ * You shouldn't need to edit anything below here - it's just a demo after all
+ */
+
+session_start();
+
+if ( isset( $_GET[ 'reset' ] ) )
+{
+    unset( $_SESSION[ 'token' ] );
+    header('Location: /');
+    exit;
+}
+elseif ( isset( $_SESSION['token'] ) )
+{
+    print_r($_SESSION['token']);
+    echo '<a href="/?reset=1">Reset</a>';
+}
+elseif ( isset( $_GET['code'] ) )
+{
+    if (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+
+        unset($_SESSION['oauth2state']);
+        exit('Invalid state');
+
+    }
+
+    $_SESSION['token'] = $provider->getAccessToken('authorization_code', [
+        'code' => $_GET['code']
+    ]);
+    header('Location: /');
+    exit;
+}
+elseif ( isset( $_GET['error'] ) )
+{
+    echo htmlspecialchars( $_GET[ 'error_description' ] );
+    echo '<a href="/">Try again</a>';
+    exit;
+}
+else
+{
+    $authUrl = $provider->getAuthorizationUrl( [
+        'scope' => [
+            'write_products'
+        ],
+    ]);
+    $_SESSION['oauth2state'] = $provider->getState();
+    header('Location: '.$authUrl);
+    exit;
+}
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Kong Provider for OAuth 2.0 Client
+
+This package provides Kong OAuth 2.0 support for the PHP League's [OAuth 2.0 Client](https://github.com/thephpleague/oauth2-client).
+
+## Requirements
+
+The following versions of PHP are supported.
+
+* PHP 5.5
+* PHP 5.6
+* PHP 7.0
+* HHVM
+
+## Installation
+
+Add the following to your `composer.json` file.
+
+> **Note:** Once version 1.0 of the [OAuth 2.0 Client](https://github.com/thephpleague/oauth2-client) is released, you'll be able to install from composer without the `@dev` minimum stability flag.
+
+```json
+{
+    "require": {
+        "kong/oauth2-kong-php-client": "~0.0@dev",
+        "league/oauth2-client": "~1.0@dev"
+    }
+}
+```
+
+> **Note:** OAuth 2.0 Client v1.0 is still in dev mode so you'll need to require it explicitly in your require using the `@dev` minimum stability flag since [composer won't pull in a dev mode dependency of a dependency](https://getcomposer.org/doc/faqs/why-can%27t-composer-load-repositories-recursively.md).
+
+
+## Usage
+
+### Authorization Code Flow
+
+See example/index.php
+
+## License
+
+The MIT License (MIT). Please see [License File](https://github.com/thephpleague/oauth2-facebook/blob/master/LICENSE) for more information.

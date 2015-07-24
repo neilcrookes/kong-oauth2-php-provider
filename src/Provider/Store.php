c302b1167c52b7e8bf57e3e633f9676d73e32cb9
Neil Crookes <neil.crookes@parcelmonkey.com>
2015-07-24 11:34:15 +0100

    Adding methods to retrieve store owner details

--- src/Provider/Store.php
+++ src/Provider/Store.php
@@ -10,8 +10,36 @@ class Store extends GenericResourceOwner implements ResourceOwnerInterface
     /**
      * @return mixed
      */
-    public function getName()
+    public function getStoreName()
     {
         return $this->response[ 'name' ] ?: null;
+    }
+    /**
+     * @return mixed
+     */
+    public function getOwnerFirstName()
+    {
+        return $this->response[ 'owner' ][ 'first_name' ] ?: null;
+    }
+    /**
+     * @return mixed
+     */
+    public function getOwnerLastName()
+    {
+        return $this->response[ 'owner' ][ 'last_name' ] ?: null;
+    }
+    /**
+     * @return mixed
+     */
+    public function getOwnerName()
+    {
+        return $this->response[ 'owner' ][ 'name' ] ?: null;
+    }
+    /**
+     * @return mixed
+     */
+    public function getOwnerEmail()
+    {
+        return $this->response[ 'owner' ][ 'email' ] ?: null;
     }
 }
